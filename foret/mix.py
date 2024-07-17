f30c46b59048b5e584ed5a50fd90a08784fc23b6
Pascal Acco <pacco@spacer.laas.fr>
2024-07-17 13:45:56 +0200

    albre

---
+++ foret/mix.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from foret.albre import Albre
+
+technologies_connues = ('eolienneON', 'eolienneOFF', 'panneauPV', 'methanation', 'centraleNuc', 'biomasse', 'EPR2')
+
+
+class Mix(Albre):
+    def __init__(self, name=None, items_calculables=technologies_connues):
+        super().__init__(name=name, items_calculables=items_calculables)
+
+
+if __name__ == "__main__":
+
+    Eu = Mix("EU")
+    Fr = Mix("FR")
+    metro = Mix("metro")
+    metro.from_json("flaskapp/game_data/mix_init.json")
+    metro.to_json("flaskapp/game_data/fr_metro_init.json")
+    metro.from_json("flaskapp/game_data/fr_metro_init.json")
+    metro.to_json("flaskapp/game_data/fr_metro_init.json")
+    Fr &= metro
+    Eu &= Fr
+    print(Eu)
+    Eu.to_json("flaskapp/game_data/eu_init.json")
+    print(dict(Eu))
+    print(Eu@["EU", "FR", "metro", "hdf"])
+    loc = "FR@metro@hdf@biomasse"
+    print(Eu@loc)
+    Eu -= Eu
+    print(Eu)
---
+++ foret/prospecteur.py
@@ -0,0 +1,25 @@
+"""Le prospecteur imagine des scénari du futur
+
+Le prospecteur envisage différentes décisions en fonction de différents
+aléas et avance dans un de ces futurs nommés "futurible"
+
+Techniquement à chaque pas dans le temps (5ans) des aléas arrivent :
+    - événement de maintenance : fin de vie d'une centrale
+    - aléa climatique (sécheresse et impact sur nucléaire, biomasse...)
+    - aléa politique (demande électrique fluctue si avion électrique, chauffage pompe à chaleur, ...)
+"""
+
+
+class Futuribles:
+    """Gère et stocke un scénario envisagé
+
+    Techniquement, une liste temporelle d'
+       - événements entrants
+       - aléas entrants
+       - décisions de production
+    """   
+
+
+
+if __name__=="__main__":
+   pass
