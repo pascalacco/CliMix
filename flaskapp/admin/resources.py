fdf257bdad0c4032b4b4876e24ccf2f5e7cab426
Pascal Acco <pacco@spacer.laas.fr>
2025-03-26 07:29:36 +0100

    tableau de bord

---
+++ flaskapp/admin/resources.py
@@ -0,0 +1,62 @@
+""" ADMIN blueprint """
+
+import os
+import sys,json
+sys.path.append('../')
+from flask import Blueprint, Flask,render_template, session,request, redirect, url_for
+from flask import current_app
+
+from flaskapp.archiveur import Parties
+
+# Creation du Blueprint l'administration
+admin_blueprint = Blueprint('admin', __name__)
+
+partie = Parties()
+
+def normap(value, start1, stop1, start2, stop2):
+    OldRange = (stop1 - start1)
+    if OldRange == 0:
+        NewValue = start2
+    else:
+        NewRange = (stop2 - start2)
+        NewValue = (((value - start1) * NewRange) / OldRange) + start2
+
+    return NewValue
+
+
+def get_group_list():
+    grouplist = Parties().get_liste_equipes()
+
+    for equipe in grouplist:
+        for partie in grouplist[equipe] :
+            percent = normap(int(partie['data']), 2030, 2050, 0, 100)
+            partie['percent'] = percent
+
+    return grouplist
+
+
+@admin_blueprint.route('/admin/dashboard')
+def dashboard(method=['GET']):
+    
+    if 'username' in session and 'admin_climix_man' in session['attributes']['memberOfCN']:
+        grouplist = get_group_list()
+        return render_template('tableau_de_bord.html', username=session['username'], attributes=session['attributes']['memberOfCN'],grouplist=grouplist)
+    return 'Login required. <a href="/admin/login">Login</a>', 403
+
+@admin_blueprint.route('/admin/comparer/<groupe>', methods=["POST"])
+def comparer(groupe):
+    return(render_template('comparer.html'))
+
+
+@admin_blueprint.route('/admin/tableau_de_bord')
+def tableau_de_bord(method=['GET']):
+    grouplist = partie.get_liste_groupes_par_parties()
+    print(grouplist)
+    return render_template('tableau_de_bord.html',
+                           username="Admin",
+                           attributes='Public',
+                           grouplist=grouplist)
+
+@admin_blueprint.route('/admin/view/<equipe>/<partie>')
+def view(equipe, partie):
+    return(render_template('view.html', equipe=equipe, partie=partie))
---
+++ flaskapp/static/tableau_de_bord.js
@@ -0,0 +1,38 @@
+function neste_les_checks() {
+
+    checkgroups = document.querySelectorAll('[id*=check-].Option');
+    for(var j=0; j<checkgroups.length; j++) {
+        checkgroup = checkgroups[j]
+        checkgroup.checkFils = document.querySelectorAll('[id*='+checkgroup.id+'].subOption')
+        checkgroup.onclick = function() {
+            for(var i=0; i<this.checkFils.length; i++) {
+                this.checkFils[i].checked = this.checked;
+
+            }
+        }
+        for(var i=0; i<checkgroup.checkFils.length; i++)
+        {
+            checkgroup.checkFils[i].checkPere = checkgroup
+            checkgroup.checkFils[i].onclick = function() {
+                this.checkPere.checkedCount = document.querySelectorAll('[id*='+this.checkPere.id+'].subOption:checked').length;
+                this.checkPere.checked = this.checkPere.checkedCount > 0;
+                this.checkPere.indeterminate = this.checkPere.checkedCount > 0 && this.checkPere.checkedCount < this.checkPere.checkFils.length;
+                }
+        }
+    }
+};
+
+function go_to_comparer(groupe)
+{
+    var checked_list={}
+    fetch("/admin/comparer/" + groupe, {
+        redirect: 'follow',
+        method: "POST",
+        body: JSON.stringify(checked_list),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        });
+};
+
+$(neste_les_checks());
+
+
