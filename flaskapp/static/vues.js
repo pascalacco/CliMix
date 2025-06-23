db76a9fc8ed8823b7380928dac337c7b5ccf66fb
Pascal Acco <pacco@spacex>
2025-06-23 17:26:19 +0200

    page journal qu'en fin de tour

--- flaskapp/static/vues.js
+++ flaskapp/static/vues.js
@@ -3,9 +3,12 @@ $(function () {
         $('#productionBar').html('<span class="spinner-border spinner-border-sm"></span>&nbsp;&nbsp;Production');
         location.href = "/vues/production";
     });
+    
 });
 
 function spin(me){
     $('#'+me).html('<span class="spinner-border spinner-border-sm"></span>&nbsp;&nbsp;'+me);
     location.href = "/vues/"+equipe+"/"+partie+"/"+annee+"/"+me;
 }
+
+
